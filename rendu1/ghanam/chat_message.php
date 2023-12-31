29aee102f6e6286fde0acf947349054ff3d1db00
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2023-12-31 15:20:46 +0100

    create chat msg

---
+++ rendu1/ghanam/chat_message.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class=" w-2/5 h-96 overflow-auto rounded bg-pink-500 p-2">
+    <div class="w-fit rounded-md bg-pink-700 text-white p-1 m-1 boder" id="conntent"></div>
+   </div>
+
+   <div class="w-2/5 h-1/5 bg-sky-400">
+    <input type="text" placeholder="enter message" class="w-4/5 h-14" id="send_message" required>
+    <input type="submit" value="send" id="send">
+   </div>
+    
+</body>
+</html>
